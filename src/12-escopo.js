a0888519788b1f4df7a3b434a16d236316a98809
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-18 11:21:32 -0300

    feat: understanding scope with var, let and const variables

---
+++ src/12-escopo.js
@@ -0,0 +1,14 @@
+var animal = 'cat'
+console.log(animal)
+
+let sound = 'meow'
+const nameCat = 'Jake'
+
+console.log(sound, nameCat)
+
+{
+    let animal = 'dog'
+    console.log(animal)
+}
+
+console.log(animal)

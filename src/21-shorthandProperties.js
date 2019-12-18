cabd32b2d4b18eaf505ca0d76453d6f6e235c41d
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-18 18:16:41 -0300

    feat: understending shorthand properties

---
+++ src/21-shorthandProperties.js
@@ -0,0 +1,17 @@
+//Shorthand Properties
+
+let firstName = 'Daniel'
+let surname = 'Silva'
+let age = 32
+
+const person = {
+    firstName,
+    surname,
+    age,
+
+    hello() {
+        console.log('Hello')
+    }
+}
+
+console.log(person, person.hello())

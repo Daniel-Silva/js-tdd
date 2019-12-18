207f3294981711a8ab5b6fb0e219137f2467b066
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-18 18:28:58 -0300

    feat: new methods for string

---
+++ src/23-newMethodString.js
@@ -0,0 +1,21 @@
+let text = 'Lorem ipsum dolor sit amet'
+
+// startsWith()
+console.log(text.startsWith('L')) // Verifica se a string começa com o parâmetro informado no método startsWith()
+
+//OU
+
+console.log(text.startsWith('rem', 2))
+
+//endsWith()
+console.log(text.endsWith('t')) // Verifica se a string termina com o parâmetro informado no método endsWith()
+
+//OU
+
+console.log(text.endsWith('ame', 25))
+
+// repeat()
+console.log('lo'.repeat(10)) // Repete a string na quantidade de vezes informada por parâmetro no método repeat()
+
+// includes()
+console.log(text.includes('L')) // Verifica se a string contém o parâmetro informado no método includes()

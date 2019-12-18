02e5defdc0d4b51d09f7d6addfacac13e56f7b94
Daniel Silva <dani3l.dxp@gmail.com>
2019-12-18 18:39:55 -0300

    feat: about method Array.of()

---
+++ src/25-arrayOf.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Daniel',
+    surname: 'Silva',
+    age: 32,
+    address: {
+        publicPlace: 'Rua R. A. D. L.',
+        number: 100,
+        zipCode: '08690-750',
+        city: 'Suzano'
+    }
+}
+
+console.log(Array.of(1, 10, 'TESTE', person)) // Converte os parâmetros em um array

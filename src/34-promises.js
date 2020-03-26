64eb86f3b529ac55ace055d0dd266e40d3a1808c
Daniel Silva <dani3l.dxp@gmail.com>
2020-03-26 10:58:40 -0300

    feat: about promises

--- src/34-promises.js
+++ src/34-promises.js
@@ -11,8 +11,8 @@ let defer = new Promise((resolve, reject) => {
 
 defer.then((data) => console.log(data));
 
-const fetchs = require('node-fetch');
+// const fetchs = require('node-fetch');
 
-var clubs = fetchs('https://jsonmock.hackerrank.com/api/movies/search/?Title=substr');
+var clubs = fetch('https://jsonmock.hackerrank.com/api/movies/search/?Title=substr');
 
 clubs.then((data) => data.json()).then((data) => console.log(data)).catch((error) => console.log(error));

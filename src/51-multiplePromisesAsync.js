6c74b01e003ddf19f0b8e5a82755b4dd199e0d97
Daniel Silva <dani3l.dxp@gmail.com>
2020-04-08 09:06:37 -0300

    feat: multiple promises with async/await

--- src/51-multiplePromisesAsync.js
+++ src/51-multiplePromisesAsync.js
@@ -1,15 +1,8 @@
 async function getZipCode(ceps) {
-	// return fetchs('https://api.cartolafc.globo.com/mercado/destaques')
-	// 	.then((data) => data.json())
-	// 	.then((data) => console.log(data))
-	// 	.catch((error) => console.log(error));
-
 	try {
-		//const fetchs = require('node-fetch');
-		const promises = ceps.map((cep) => fetch(`https://viacep.com.br/ws/${cep}/json/`).then((r) => r.json()));
-
+		const fetchs = require('node-fetch');
+		const promises = ceps.map((cep) => fetchs(`https://viacep.com.br/ws/${cep}/json/`).then((r) => r.json()));
 		const adresess = await Promise.all(promises);
-
 		console.log(adresess);
 	} catch (err) {
 		console.log('NOOOOOOOOO, we got an error!', err);

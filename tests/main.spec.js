58a23995b84fb4129e09e2964dc34c7617e64515
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-20 16:09:21 -0300

    feat: implementing a simple calc and applying tdd

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,31 +1,59 @@
-describe('Main', function() {
-	describe('Method A', function() {
-		context('Case A', function() {
-			let arr;
-			// roda UMA VEZ antes do bloco
-			before(function() {});
-			// roda UMA VEZ depois do bloco
-			after(function() {});
-			// roda todas as vezes, antes de CADA bloco
-			beforeEach(function() {
-				arr = [ 1, 2, 3 ];
-			});
-			// roda todas as vezes, depois de CADA bloco
-			afterEach(function() {});
+const expect = require('chai').expect;
+const calc = require('../src/Mod#4 - Concept and Practice/calc')
 
-			it('should have a size of 4 when push another value to the array', function() {
-				arr.push(4);
-				console.log(arr.length);
-			});
+describe('Calc', function() {
+    // smoke tests
+    describe('Smoke tests', function() {
+        it('should exist the calc lib', function() {
+            expect(calc).to.exist;
+        })
 
-			it('should have a size of 2 when pop a value from the array', function() {
-				arr.pop();
-				console.log(arr.length);
-			});
+        it('should exist the method `sum`', function() {
+            expect(calc.sum).to.exist;
+            expect(calc.sum).to.be.a('function');
+        })
 
-			it('should remove 3 when use pop in the array', function() {
-				console.log(arr.pop() === 3);
-			});
-		});
-	});
+        it('should exist the method `sub`', function() {
+            expect(calc.sub).to.exist;
+            expect(calc.sub).to.be.a('function');
+        })
+
+        it('should exist the method `mult`', function() {
+            expect(calc.mult).to.exist;
+            expect(calc.mult).to.be.a('function');
+        })
+
+        it('should exist the method `div`', function() {
+            expect(calc.div).to.exist;
+            expect(calc.div).to.be.a('function');
+        })
+    });
+
+    describe('Sum', function() {
+        it('should return 4 when `sum(2,2)`', function () {
+            expect(calc.sum(2,2)).to.be.equal(4)
+        })
+    })
+    
+    describe('Sub', function() {
+        it('should return 4 when `sub(2,6)`', function () {
+            expect(calc.sub(6,2)).to.be.equal(4)
+        })
+    })
+    
+    describe('Mult', function() {
+        it('should return 4 when `sum(2,2)`', function () {
+            expect(calc.mult(2,2)).to.be.equal(4)
+        })
+    })
+
+    describe('Div', function() {
+        it('should return 4 when `sum(16,4)`', function () {
+            expect(calc.div(16,4)).to.be.equal(4)
+        })
+
+        it('should return `Não é possível divisão por zero!` when divide by 0', function () {
+            expect(calc.div(16,0)).to.be.equal(`Não é possível divisão por zero!`)
+        })
+    })
 });

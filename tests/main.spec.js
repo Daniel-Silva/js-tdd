acd099b9231212f4a94613235b6513ec4e83868f
Daniel Silva <dani3l.dxp@gmail.com>
2020-05-20 11:09:04 -0300

    feat: mocha operation - describe(), context() and it()

---
+++ tests/main.spec.js
@@ -0,0 +1,14 @@
+describe('Main', function() {
+	describe('Method A', function() {
+		context('Case A', function() {
+			it('sthe sum of two integer values should happen', function() {
+				// espera que aconteça
+				// entra de dados / método sum(2,2)
+				// espera retornar (4) => true
+				// ou (3) => broken test
+			});
+		});
+
+		context('Case B', function() {});
+	});
+});
